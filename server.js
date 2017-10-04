80ddcbec7fa7187c90c502da0b0f6b1cc8aa1138
Margo Schaedel <margoschaedel@gmail.com>
2017-10-03 22:35:58 -0600

    Add get requests to retrieve palettes and projects from database

--- server.js
+++ server.js
@@ -11,8 +11,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (request, response) => {});
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Palette Picker';
 
-app.listen(3000, () => {
-  console.log("Express running on localhost:3000");
+// ENDPOINTS
+
+// retrieve all projects
+app.get('/api/projects', (request, response) => {
+  database('projects').select()
+    .then( projects => {
+      if (!projects.length) {
+        return response.status(404).json({ error: 'No projects were found' })
+      }
+      response.status(200).json(projects);
+    })
+    .catch( error => {
+      response.status(500).json({ error });
+    })
+})
+
+// retrieve all palettes
+app.get('/api/palettes', (request, response) => {
+  database('palettes').select()
+    .then( palettes => {
+      if (!palettes.length) {
+        return response.status(404).json({ error: 'No palettes were found' })
+      }
+      response.status(200).json(palettes);
+    })
+    .catch( error => {
+      response.status(500).json({ error });
+    })
+})
+
+
+// create and save a new project folder
+
+// save a palette to database
+
+// delete a palette
+
+// delete a project (also deletes palettes)
+
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
