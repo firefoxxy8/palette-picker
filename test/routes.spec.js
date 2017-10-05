40c9551350fb76d7016bef6b7fa5657281a05476
Margo Schaedel <margoschaedel@gmail.com>
2017-10-05 17:19:44 -0600

    Add test for clientside routes, tests successfully passing

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,9 +6,29 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  
+  it('should return the homepage with text', (done) => {
+    chai.request(server)
+    .get('/')
+    .end( (error, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.include('Palette Picker');
+      done();
+    });
+  });
+
+  it('should return a 404 route that does not exist', (done) => {
+    chai.request(server)
+    .get('/eureka')
+    .end( (error, response) => {
+      response.should.have.status(404);
+      done();
+    });
+  });
 });
 
 describe('API Routes', () => {
 
+
+  
 });
